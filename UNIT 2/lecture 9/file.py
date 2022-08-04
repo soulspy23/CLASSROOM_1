bd739370bd5c5c6463a9c7579f9618c25fdd696b
SOUL SPY <shravani.yp@somaiya.edu>
2022-08-04 19:59:47 +0530

    c

--- UNIT 2/lecture 9/file.py
+++ UNIT 2/lecture 9/file.py
@@ -1,8 +1,22 @@
 #file handling
 
+print("File 1 :")
 f = open("f.txt", "w") #write in file
 f.write("Hello World !")
 f=open("f.txt", "r") #read from file
 
 print("Reading 7 characters from file :", f.read(7)) #read 7 characters
 print("Reading whole line :",f.readline()) #read whole line
+
+f.close() #close file
+
+#looping through file
+print("\nFile 2 :")
+f = open("f1.txt", "w")
+f.write("I am Shravani Patil.")
+f.write("I am in SYBCA.")
+f.close()
+
+print("File 2 looping :")
+for x in open("f1.txt"):
+    print((x))
