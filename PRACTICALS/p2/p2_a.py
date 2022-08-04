d4d83a5faa0088568eaab90ca51e1626015b6062
SOUL SPY <shravani.yp@somaiya.edu>
2022-08-04 20:34:53 +0530

    p2

--- PRACTICALS/p2/p2_a.py
+++ PRACTICALS/p2/p2_a.py
@@ -1,6 +1,10 @@
 #18th July
 # Write a function that takes a character (i.e. a string of length 1) and returns Trueif it is a vowel, False otherwise.
 
+print("**************************************************")
+print("Program to detect if a character is a vowel or not")
+print("**************************************************\n")
+
 def vowels(ch):
     if (ch == "a" or ch == "A" or ch == "e" or ch == "E" or ch == "i" or ch == "I" or ch == "o" or ch == "O" or ch == "u" or ch == "U"):
         return True
--- PRACTICALS/p2/p2_b.py
+++ PRACTICALS/p2/p2_b.py
@@ -1,5 +1,9 @@
 # Define a function that computes the length of a given list or string.
 
+print("************************************************")
+print("Program to compute the length of list and string")
+print("************************************************\n")
+
 def str_len(str):
     str = "i study in SYBCA"
     print("the length of string is", len(str))
